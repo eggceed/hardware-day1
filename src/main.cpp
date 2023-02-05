127a47294f2c4621ab3700ea75d85ad544b7c52e
GToidZ <39559733+GToidZ@users.noreply.github.com>
2023-02-05 13:23:39 +0700

    feat: initialize new repo

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+// Define pins
+#define BTN 27
+#define RED 26
+#define YELLOW 25
+#define GREEN 33
+#define LDR 32
+
+#define MIN_LDR 0
+#define MAX_LDR 4095
+
+int bits[3] = {0, 0, 0};
+int pins[3] = {RED, YELLOW, GREEN};
+
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(0, 5000, 8); // Main channel
+  ledcSetup(1, 5000, 8); // Voided channel
+  ledcAttachPin(RED, 0);
+  ledcAttachPin(YELLOW, 0);
+  ledcAttachPin(GREEN, 0);
+}
+
+void loop() {
+  long pwm = map(analogRead(32), 0, 4095, 0, 255);
+  // Display LED
+  ledcWrite(0, pwm);
+  for (int i = 0; i < 3; i++) {
+    if (!bits[i]) continue; 
+  }
+  delay(100);
+}
